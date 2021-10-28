4f62a0620976e4146f96c2cae0049171132d47cb
juanan <juanan318@gmail.com>
2021-10-28 19:48:33 +0200

    Moving hit array from std::vector to C style pointer, issues found while saving the data in the tree

--- ANA/AnaTDS.cxx
+++ ANA/AnaTDS.cxx
@@ -8,12 +8,10 @@
 
 #include <TFile.h>
 #include <TTree.h>
-#include <Rtypes.h>
 
 void AnaTDS::analizeTDS(const std::string &fileRaw, const std::string &fileRoot, int &index){
 
   std::ifstream fin (fileRaw, std::ios::in | std::ios::binary);
-  int DCPOINTS = 50;
 
   int pulse_depth;
   double srate;
@@ -24,7 +22,7 @@ void AnaTDS::analizeTDS(const std::string &fileRaw, const std::string &fileRoot,
   uint64_t prevTime=0;
   uint8_t mVdiv[4];
 
-  std::vector<Hit *> myHits;
+  Hit * myHits[4]={nullptr,nullptr,nullptr,nullptr};
 
   int nEvents=0, nHits=0, size=0;
   bool first = true;
@@ -39,13 +37,13 @@ void AnaTDS::analizeTDS(const std::string &fileRaw, const std::string &fileRoot,
         nHits = blockhead.NHits/blockhead.NEvents;
         srate = blockhead.SRate;
 	pulse_depth = blockhead.PSize;
-        myHits.resize(nHits);
         std::cout<<"nEvents "<<nEvents<<" NHits "<<nHits<<std::endl;
         std::cout<< "SRATE: " << (double)srate << " PULSE DEPTH: " << pulse_depth<<" PRETRIGGER: "<< blockhead.Pretrigger << std::endl;
         for(int i=0; i<nHits;i++){
+          std::cout<<"mVdiv "<<(int)blockhead.mVdiv[i]<<std::endl;
           std::cout<<"Polarity ch"<<i+1<<" "<<(int)blockhead.NegPolarity[i]<<std::endl;
           std::string hitName = "Hit"+std::to_string(i);
-          tree->Branch(hitName.c_str(), "Hit", &myHits[i] /*, 32000, 99*/);
+          tree->Branch(hitName.c_str(), &myHits[i] /*, 32000, 99*/);
         }
         first=false;
       } else {
@@ -62,24 +60,33 @@ void AnaTDS::analizeTDS(const std::string &fileRaw, const std::string &fileRoot,
         fin.read((char*)&eventhead,sizeof(ANAEventHead));
         size += sizeof(ANAEventHead);
         //std::cout<<"File position "<<fin.tellg()<<" "<<size<<std::endl;
-        //if(c<nEvents)std::cout<<"Clocks "<<c<<" "<<eventhead.clockTicksLT<<" "<<eventhead.clockTicksRT<<std::endl;
+        //if(index%nEvents ==0)std::cout<<"Clocks "<<index<<" "<<(double)eventhead.clockTicksLT<<" "<<(double)eventhead.clockTicksRT<<std::endl;
         if(fin.peek() == EOF)break;
 
-        std::vector <int8_t>buffer(pulse_depth);
+        std::vector <Char_t>buffer(pulse_depth);
 
           for(int i=0; i<nHits;i++){
             fin.read((char *)&buffer[0],pulse_depth);
             size += pulse_depth;
             if(fin.peek() == EOF)break;
-            //std::transform(buffer.begin(), buffer.end(), std::back_inserter(sData), [](int8_t x) { return (Short_t)x;});
-            Hit hit(index,pulse_depth,blockhead.mVdiv[i], srate, blockhead.Pretrigger,blockhead.NegPolarity[i], buffer);
-            myHits[i] =&hit;
+
+              if(myHits[i]){
+                delete myHits[i];
+                myHits[i] = nullptr;
+              }
+
+            myHits[i] = new Hit(i,index,pulse_depth,blockhead.mVdiv[i], srate, blockhead.Pretrigger,blockhead.NegPolarity[i], buffer);
+
             myHits[i]->SetClockTickLT (eventhead.clockTicksLT);
             myHits[i]->SetClockTickRT (eventhead.clockTicksRT);
             // Set delta time (real time since previous event)
-            myHits[i]->SetDeltaTime ( eventhead.clockTicksRT - prevTime);
-            prevTime = myHits[0]->GetClockTickRT();
+              if(index==0)myHits[i]->SetDeltaTime(0);
+              else myHits[i]->SetDeltaTime ( eventhead.clockTicksRT - prevTime);
+
+            if(i==nHits-1)prevTime = eventhead.clockTicksRT;
             myHits[i]->analyzeHit();
+
+            //if(index%nEvents ==0)std::cout<<"Clocks "<<index<<" "<<(double)myHits[i]->GetClockTickLT()<<" "<<(double)myHits[i]->GetClockTickLT()<<std::endl;
           }
         index++;
         tree->Fill();
